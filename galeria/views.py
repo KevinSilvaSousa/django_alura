5f8527111e2f341725ad8c2dbabb3b9e0f2edbc5
Kevin Sousa <ksilvasousa2008@gmail.com>
2026-02-18 17:06:09 -0300

    Commits

--- galeria/views.py
+++ galeria/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 
 
+
 # Create your views here.
 
 def index(request):
-    return render(request, 'galeria/index.html')
+    dados = {
+        1: {"nome": "Nebulosa de Carina", "legenda": "webbtelecope.org / NASA / James Webb"},
+        2: {"nome": "Galaxia NGC 1079",
+        "legenda": "nasa.or / NASA / Hubble"}
+}
+    return render(request, 'galeria/index.html', {"cards": dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
