e15d57fc4fe619016047d596d6925dd0ed35d8e7
Kevin Sousa <ksilvasousa2008@gmail.com>
2026-02-25 16:46:59 -0300

    Commits

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from galeria.views import index, imagem
+from galeria.views import index, imagem, buscar
 
 urlpatterns = [
     path('', index, name='index.html'),
     path('imagem/<int:foto_id>', imagem, name='imagem.html'),
+    path("buscar", buscar, name="buscar"),
 ]
