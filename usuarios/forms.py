e749abe0664930853326cc9d73bab1ce2801315a
Kevin Sousa <ksilvasousa2008@gmail.com>
2026-03-02 16:13:08 -0300

    Commit

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -4,12 +4,73 @@ class LoginForms(forms.Form):
     nome_login = forms.CharField(
         label="Nome de Login",
         required=True,
-        max_length=100
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Digite o seu nome"
+            }
+        )
+
     )
     senha=forms.CharField(
         label="senha",
         required=True,
         max_length=70,
-        widget=forms.PasswordInput()
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Digite a sua sennha"
+            }
+        )
+
+    )   
+
+class CadastroForms(forms.Form):
+    nome_cadastro = forms.CharField(
+        label="Nome de Cadastro",
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Digite o seu nome"
+            }
+        )
+
+    )
+    email = forms.EmailField(
+        label="Nome de Cadastro",
+        required=True,
+        max_length=100,
+        widget=forms.EmailInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "email@gmail.com"
+            }
+        )
+    )
+    senha1=forms.CharField(
+        label="senha",
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Digite a sua sennha"
+            }
+        )
+
+    )   
+    senha2=forms.CharField(
+        label="Confime a sua senha",
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Digite a sua sennha novamente"
+            }
+        )
 
     )   
